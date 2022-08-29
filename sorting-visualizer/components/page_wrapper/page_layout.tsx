b1b9dfadfdc387e6478d21d50a464b9eaadc8765
nutdanai-kpjr <nutdanai.kpjr@gmail.com>
2022-08-30 01:56:42 +0700

    Update page layout styling

--- sorting-visualizer/components/page_wrapper/page_layout.tsx
+++ sorting-visualizer/components/page_wrapper/page_layout.tsx
@@ -1,13 +1,17 @@
 import FooterBar from "./footer_bar";
 import NavBar from "./nav_bar";
+import styles from "../../styles/PageLayout.module.css";
 
-
-export default function PageLayout({ children }: { children: React.ReactNode }) {
-    return (
-<>
-<NavBar />
-<main>{children}</main>
-<FooterBar />
-</>
-    );
-  }
+export default function PageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <NavBar />
+      <main className={styles.container}>{children}</main>
+      <FooterBar />
+    </>
+  );
+}
