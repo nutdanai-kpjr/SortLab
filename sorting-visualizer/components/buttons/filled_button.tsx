2aba1c6c46b152a9adee8803c929e51fcbb3a5eb
nutdanai-kpjr <nutdanai.kpjr@gmail.com>
2022-09-02 12:58:26 +0700

    Add landing page

--- sorting-visualizer/components/buttons/filled_button.tsx
+++ sorting-visualizer/components/buttons/filled_button.tsx
@@ -1,3 +1,15 @@
-export default function FilledButton() {
-  return <div>Footer Bar</div>;
+import { NextPage } from "next";
+
+interface Props {
+  title: string;
+  onClick?: () => void;
 }
+
+const FilledButton: NextPage<Props> = ({ title, onClick }) => {
+  // using destructuring to get username
+
+  return <button onClick={onClick}>{title}</button>;
+};
+
+// export component
+export default FilledButton;
