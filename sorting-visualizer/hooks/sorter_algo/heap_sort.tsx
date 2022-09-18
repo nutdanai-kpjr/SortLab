90c8212e33d793f506f38affa1adda4627c3fb65
nutdanai-kpjr <nutdanai.kpjr@gmail.com>
2022-09-18 17:53:03 +0700

    Add heap sort stop sort function

--- sorting-visualizer/hooks/sorter_algo/heap_sort.tsx
+++ sorting-visualizer/hooks/sorter_algo/heap_sort.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useRef } from "react";
 import { ArrayCtx } from "../../context/arrayContext";
-import { COLORS } from "../../styles/color";
+import { COLORS, getGradientColor, getRandomColor } from "../../styles/color";
 import { Item, SortAlgorithm } from "../sorter_abstract";
 
 export const useHeapSort: () => SortAlgorithm = () => {
@@ -11,6 +11,8 @@ export const useHeapSort: () => SortAlgorithm = () => {
     stopSort,
     updateColor,
     updateDifferentColor,
+    blinkItemDifferentColor,
+    replaceItem,
   } = useContext(ArrayCtx);
 
   const info = {
@@ -28,14 +30,107 @@ export const useHeapSort: () => SortAlgorithm = () => {
   // 2.1 Swap root with last element
   // 2.2 Heapify root
   // 2.3 Repeat 2.1 and 2.2 until heap is empty
-
   class MinHeap {
     heap: Item[];
-
     constructor() {
       this.heap = [];
     }
 
+    async add(arrIndex: number) {
+      // index of item in itemArrayRef (not heap) (use to update color)
+      let item = itemArrayRef.current[arrIndex];
+      let itemHeapLevel = this.getHeapLevel(arrIndex);
+      let color = getGradientColor(itemHeapLevel + 1); //gradient level start from 1st level
+      await updateColor([arrIndex], color);
+      this.heap.push(item);
+      await this.heapifyUp(); // to move the new node up to the correct position
+    }
+
+    async remove(): Promise<Item | undefined> {
+      if (this.heap.length === 0) return;
+      if (this.heap.length === 1) return this.heap.pop();
+      // 1. Remove root
+      // 2. Replace root with last element
+      // 3. Heapify down to move the new root to the correct position
+      let rootItem = this.heap.shift();
+      let lastItem = this.heap.pop();
+      if (lastItem) this.heap.unshift(lastItem);
+
+      await this.heapifyDown();
+
+      return rootItem;
+    }
+
+    async heapifyUp() {
+      let lastItemIndex = this.heap.length - 1;
+      let parentOfLastItemIndex = this.getParentIndex(lastItemIndex);
+      // await blinkItemDifferentColor(
+      //   [
+      //     { index: lastItemIndex, color: COLORS.SPECIAL },
+      //     { index: parentOfLastItemIndex, color: COLORS.SPECIAL },
+      //   ],
+      //   COLORS.COMPARE,
+      //   3
+      // );
+
+      while (
+        lastItemIndex > 0 &&
+        this.isSmaller(lastItemIndex, parentOfLastItemIndex)
+      ) {
+        // we want min at top , so swap if child is smaller than parent
+        this.swap(lastItemIndex, parentOfLastItemIndex);
+        lastItemIndex = parentOfLastItemIndex;
+        parentOfLastItemIndex = this.getParentIndex(lastItemIndex);
+      }
+      // 1. Get last element
+      // 2. Get parent
+      // 3. Compare parent and last element
+      // 4. Swap if parent is smaller
+      // 5. Repeat 2-4 until parent is smaller than last element
+    }
+
+    async heapifyDown() {
+      // 1 Compare root with children
+      // 2 Swap if root is bigger than children
+      // 3. Repeat 3-4 until root is smaller than children
+
+      let rootIndex = 0;
+      let leftChildIndex = this.getLeftChildIndex(rootIndex);
+      let rightChildIndex = this.getRightChildIndex(rootIndex);
+
+      while (
+        this.leftChildIsSmaller(rootIndex) ||
+        this.rightChildIsSmaller(rootIndex)
+      ) {
+        // we known that at least one child is smaller than root
+        // so we need to swap root with the smaller child
+        // if there is no right child, we can just use left child as a smaller index and vice versa.
+        // but if there is both left and right child, we need to compare them to find the smaller one.
+        let smallerChildIndex: number;
+        // case 0  no child (won't be in this loop)
+        // cases 1 only have one child node
+        if (this.isHavingOneChild(rootIndex)) {
+          smallerChildIndex = this.isLeftChildEmpty(rootIndex)
+            ? rightChildIndex
+            : leftChildIndex;
+        }
+        // case 2 have two child node
+        else {
+          smallerChildIndex = this.isSmaller(leftChildIndex, rightChildIndex)
+            ? leftChildIndex
+            : rightChildIndex;
+        }
+
+        this.swap(rootIndex, smallerChildIndex);
+        rootIndex = smallerChildIndex;
+        leftChildIndex = this.getLeftChildIndex(rootIndex);
+        rightChildIndex = this.getRightChildIndex(rootIndex);
+      }
+      return;
+    }
+    swap(a: number, b: number) {
+      [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    }
     getParentIndex = (index: number) => Math.floor((index - 1) / 2);
     getLeftChildIndex = (index: number) => 2 * index + 1;
     getRightChildIndex = (index: number) => 2 * index + 2;
@@ -68,126 +163,37 @@ export const useHeapSort: () => SortAlgorithm = () => {
         this.isSmaller(this.getRightChildIndex(index), index)
       );
     };
-
-    swap(a: number, b: number) {
-      [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-    }
-    add(arrIndex: number) {
-      // index of item in itemArrayRef (not heap) (use to update color)
-      let item = itemArrayRef.current[arrIndex];
-      this.heap.push(item);
-      this.heapifyUp(); // to move the new node up to the correct position
-    }
-
-    remove(): Item | undefined {
-      if (this.heap.length === 0) return;
-      if (this.heap.length === 1) return this.heap.pop();
-      // 1. Remove root
-      // 2. Replace root with last element
-      // console.log("Heap before remove");
-      // console.table(this.heap);
-
-      let rootItem = this.heap.shift();
-      let lastItem = this.heap.pop();
-      if (lastItem) this.heap.unshift(lastItem);
-
-      // console.log("Heap after remove");
-      // console.table(this.heap);
-
-      this.heapifyDown();
-      // console.log("Heap after heapify down");
-      // console.table(this.heap);
-
-      return rootItem;
-    }
-
-    heapifyUp() {
-      let lastItemIndex = this.heap.length - 1;
-      let parentOfLastItemIndex = this.getParentIndex(lastItemIndex);
-
-      while (
-        lastItemIndex > 0 &&
-        this.isSmaller(lastItemIndex, parentOfLastItemIndex)
-      ) {
-        // we want min at top , so swap if child is smaller than parent
-        this.swap(lastItemIndex, parentOfLastItemIndex);
-        lastItemIndex = parentOfLastItemIndex;
-        parentOfLastItemIndex = this.getParentIndex(lastItemIndex);
+    getHeapLevel = (index: number) => {
+      let level = 0;
+      while (index > 0) {
+        index = this.getParentIndex(index);
+        level++;
       }
-      // 1. Get last element
-      // 2. Get parent
-      // 3. Compare parent and last element
-      // 4. Swap if parent is smaller
-      // 5. Repeat 2-4 until parent is smaller than last element
-    }
-
-    heapifyDown() {
-      // 3. Compare root with children
-      // 4. Swap if root is bigger than children
-      // 5. Repeat 3-4 until root is smaller than children
-
-      let rootIndex = 0;
-      let leftChildIndex = this.getLeftChildIndex(rootIndex);
-      let rightChildIndex = this.getRightChildIndex(rootIndex);
-      // console.log("Lefft child is smaller", this.leftChildIsSmaller(rootIndex));
-      // console.log(
-      //   "Right child is smaller",
-      //   this.rightChildIsSmaller(rootIndex)
-      // );
-
-      while (
-        this.leftChildIsSmaller(rootIndex) ||
-        this.rightChildIsSmaller(rootIndex)
-      ) {
-        // we known that at least one child is smaller than root
-        // so we need to swap root with the smaller child
-        // if there is no right child, we can just use left child as a smaller index and vice versa.
-        // but if there is both left and right child, we need to compare them to find the smaller one.
-        let smallerChildIndex: number;
-        // case 0  no child (won't be in this loop)
-        // cases 1 only have one child node
-        if (this.isHavingOneChild(rootIndex)) {
-          smallerChildIndex = this.isLeftChildEmpty(rootIndex)
-            ? rightChildIndex
-            : leftChildIndex;
-        }
-        // case 2 have two child node
-        else {
-          smallerChildIndex = this.isSmaller(leftChildIndex, rightChildIndex)
-            ? leftChildIndex
-            : rightChildIndex;
-        }
-
-        this.swap(rootIndex, smallerChildIndex);
-
-        // console.log("Heap after swap");
-        // console.table(this.heap);
-
-        rootIndex = smallerChildIndex;
-        leftChildIndex = this.getLeftChildIndex(rootIndex);
-        rightChildIndex = this.getRightChildIndex(rootIndex);
-      }
-      return;
-    }
+      return level;
+    };
   }
 
   const sort = async () => {
     let arr: Item[] = [...itemArrayRef.current];
     let sorted: Item[] = [];
     let minHeap = new MinHeap();
-    for (let i = 0; i < arr.length; i++) {
-      minHeap.add(i);
-    }
-
-    // console.log("HeapLength", minHeap.heap.length);
-    // console.log("ArrLength", arr.length);
 
     for (let i = 0; i < arr.length; i++) {
-      let item = minHeap.remove();
-      // console.log("loop", i);
-      if (item) sorted.push(item);
+      if (isStopRef.current) return await stopSort();
+      await minHeap.add(i);
     }
-    console.log(sorted);
+
+    for (let i = 0; i < arr.length; i++) {
+      if (isStopRef.current) return await stopSort();
+      let item = await minHeap.remove();
+      if (item) {
+        item = { ...item, color: COLORS.SORTED };
+        await replaceItem(i, item);
+
+        sorted.push(item);
+      }
+    }
   };
+
   return { sort, info };
 };
--- sorting-visualizer/styles/color.ts
+++ sorting-visualizer/styles/color.ts
@@ -28,3 +28,24 @@ export const getRGBAColorWIthOpacity = (r:number,g:number,b:number, opacity:numb
     
     return `rgba(${r},${g},${b},${newOpacity})`;
 }
+ 
+export const getGradientColor = (level:number,contrast=30) => {
+    if(level < 1){
+        return COLORS.BLOCKED
+    }
+    let extraFactor  =level %2 == 0? 50:0;
+    // graditent level start from 1 to ... (start from darkest color)
+    let r = 0 + level*contrast
+    let g = 0  + level*contrast
+    let b = 0 + level*contrast
+
+    // r 76 , 121 , 175, 232  // around 50 per l
+    // g 0 , 2, 1 , 15 // around 3 per level
+    // b 51, 82 ,113 ,136 // around 20 per level
+
+    // 2  4  8  16 32 64 128 256  
+    
+
+    
+    return `rgb(${r},${g},${b})`;
+}
